384b25e96a3e34bf15b1bb7744b9a631414bbdb3
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-05-02 13:28:32 -0300

    :tada: First commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Lucas Werner Kuipers on 01/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+		
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
